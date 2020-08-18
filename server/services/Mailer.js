d07222c31afe712a4b6d30c13037648021b21373
Ricardina97 <46728174+Ricardina97@users.noreply.github.com>
2020-08-18 17:59:55 +0100

    Change e-mail to more generic one

--- server/services/Mailer.js
+++ server/services/Mailer.js
@@ -6,7 +6,7 @@ class Mailer extends helper.Mail {
     constructor({ subject, recipients }, content) {
         super();
         this.sgApi = sendgrid(keys.sendGridKey);
-        this.from_email = new helper.Email('veradias97@hotmail.com');
+        this.from_email = new helper.Email('emailysendertest@gmail.com');
         this.subject = subject;
         this.body = new helper.Content('text/html', content);
         this.recipients = this.formatAddresses(recipients);
